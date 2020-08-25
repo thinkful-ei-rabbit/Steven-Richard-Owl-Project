72d2fefeea2ec4381b71e9454e97514e9923b696
Hendoe <hendo1349@gmail.com>
2020-08-25 14:12:55 -0500

    progress mark 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import participants from './participants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App(props) {
+//   const people = props.participants.map((participant) => {
+//     const alive = props.participants.filter((participant) => {
+//       let active = [];
+//       let inactive = [];
+//       if(participant.inSession) {
+//         active.push(participant);
+//     } else {
+//         inactive.push(participant);
+//     };
+//     return (
+//       <div className='box'>
+//         <span><p>{participant.name}</p></span>
+//         <span><p>{participant.name}</p></span>
+//         <span><p>{participant.name} is on Stage</p></span>
+//       </div>
+//   )
+//   })
+//   return ( <div>{people}</div> );
+// };
+
+function App(props) {
+    let people = props.participants.filter((participant) => {
+      let active = [];
+      let inactive = [];
+      if(participant.inSession) {
+        let personActive = (
+          <div className='box'>
+            <span><p>{participant.name}</p></span>
+            <span><p>{participant.name}</p></span>
+            <span><p>{participant.name} is in Session</p></span>
+          </div>
+        )
+        active.push(personActive);
+    } else {
+      let personInactive = (
+        <div className='box'>
+          <span><p>{participant.name}</p></span>
+          <span><p>{participant.name}</p></span>
+          <span><p>{participant.name} is outta Session</p></span>
+        </div>
+      )
+        inactive.push(personInactive);
+    };
+    return(active+inactive)
+  });
+  console.log(people);
+  return ( <div>{people}</div> );
+};
 
 export default App;
