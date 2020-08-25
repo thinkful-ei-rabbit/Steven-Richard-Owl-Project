735ebc79a8b28b42c181f03ebb0492790502e69a
Richard Carmen Scott <RichardCarmenScott@gmail.com>
2020-08-25 17:27:49 -0400

    added test files

--- src/People.test.js
+++ src/People.test.js
@@ -2,26 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import App from './App';
+import People from './People';
 
-describe('<App />', () => {
+describe('<People />', () => {
     it('renders without crashing', () => {
         // Create a DOM element to render the component into
         const root = document.createElement('root');
 
         // Render the component
         // If something is wrong it will fail here
-        ReactDOM.render(<App />, root);
+        ReactDOM.render(<People />, root);
 
         // Clean up
         ReactDOM.unmountComponentAtNode(root);
     });
 });
 
-describe('<App />', () => {
+describe('<People />', () => {
     it('renders this UI as expected', () => {
         // Render the component, as JSON
-        const tree = renderer.create(<App />).toJSON();
+        const tree = renderer.create(<People />).toJSON();
         // Check whether it matches the previous snapshot
         // Stored in __snapshots__/App.test.js.snap
         expect(tree).toMatchSnapshot(); 
--- src/Stage.test.js
+++ src/Stage.test.js
@@ -2,28 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import App from './App';
+import Stage from './Stage';
 
-describe('<App />', () => {
+describe('<Stage />', () => {
     it('renders without crashing', () => {
         // Create a DOM element to render the component into
         const root = document.createElement('root');
 
         // Render the component
         // If something is wrong it will fail here
-        ReactDOM.render(<App />, root);
+        ReactDOM.render(<Stage />, root);
 
         // Clean up
         ReactDOM.unmountComponentAtNode(root);
     });
 });
 
-describe('<App />', () => {
+describe('<Stage />', () => {
     it('renders this UI as expected', () => {
         // Render the component, as JSON
-        const tree = renderer.create(<App />).toJSON();
+        const tree = renderer.create(<Stage />).toJSON();
         // Check whether it matches the previous snapshot
-        // Stored in __snapshots__/App.test.js.snap
+        // Stored in __snapshots__ Stage.test.js.snap
         expect(tree).toMatchSnapshot(); 
     });
 });
